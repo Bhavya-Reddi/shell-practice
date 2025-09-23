a368c8c066a213132b4351c1159f6fb06ccd47d1
reddibhavya <reddireddibhavya@gmail.com>
2025-09-23 18:41:43 +0800

    env-variables

--- 09-data-type.sh
+++ 09-data-type.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 #EVERYTHING IN SHELL IS CONSIDERD AS STRING
 NUMBER1=100
 NUMBER2=200
-name=devops 
+NAME=devops 
 
-SUM=$(($NUMBER1+NUMBER2+name))
+SUM=$(($NUMBER1+NUMBER2+NAME))
 
 echo "sum is : ${sum}"
